2e37dcf288efba6ca75ca6d4d5f6748d8423027b
Victor Hugo Telles <tellesvictorhugo@gmail.com>
2020-09-30 00:09:43 -0300

    Commit inicial - Criação do projeto no Xcode; - Adição de ícone e Launch Screen.

---
+++ Desafio.Ioasys/Desafio.Ioasys/Desafio_IoasysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_IoasysApp.swift
+//  Desafio.Ioasys
+//
+//  Created by Victor Hugo Telles on 29/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_IoasysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Login()
+        }
+    }
+}
---
+++ Desafio.Ioasys/Desafio.Ioasys/Views/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  Desafio.Ioasys
+//
+//  Created by Victor Hugo Telles on 29/09/20.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text("Olá, mundo!")
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
