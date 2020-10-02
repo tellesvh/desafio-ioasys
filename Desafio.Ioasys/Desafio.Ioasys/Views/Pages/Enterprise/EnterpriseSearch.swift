4eb789e0807a441bfe4e489a857c3123794c8f26
Victor Hugo Telles <tellesvictorhugo@gmail.com>
2020-10-02 17:46:37 -0300

    Indicação de carregamento na lista de empresas.

--- Desafio.Ioasys/Desafio.Ioasys/Views/Pages/Enterprise/EnterpriseSearch.swift
+++ Desafio.Ioasys/Desafio.Ioasys/Views/Pages/Enterprise/EnterpriseSearch.swift
@@ -17,59 +17,61 @@ struct EnterpriseSearch: View {
     
     var body: some View {
         NavigationView {
-            VStack(spacing: 0) {
-                ZStack(alignment: .bottom) {
-                    Image("background") /* TODO Fazer overlay com símbolos Ioasys */
-                        .resizable()
-                        .frame(maxHeight: 200)
-                        .aspectRatio(1 , contentMode: .fill)
-                    VStack {
-                        HStack {
-                            Image(systemName: "magnifyingglass").foregroundColor(.gray)
-                                .padding(.trailing, 4)
-                            TextField("Pesquise por empresa...", text: $searchQuery,
-                                      onCommit: {
-                                        hasSearched = true
-                                        searchEnterprises()
-                                      })
-                                .keyboardType(.default)
-                                .font(Font.custom("Rubik", size: 16))
-                                .foregroundColor(.black)
+            ProgressViewOverlay(isLoading: $loading) {
+                VStack(spacing: 0) {
+                    ZStack(alignment: .bottom) {
+                        Image("background") /* TODO Fazer overlay com símbolos Ioasys */
+                            .resizable()
+                            .frame(maxHeight: 200)
+                            .aspectRatio(1 , contentMode: .fill)
+                        VStack {
+                            HStack {
+                                Image(systemName: "magnifyingglass").foregroundColor(.gray)
+                                    .padding(.trailing, 4)
+                                TextField("Pesquise por empresa...", text: $searchQuery,
+                                          onCommit: {
+                                            hasSearched = true
+                                            searchEnterprises()
+                                          })
+                                    .keyboardType(.default)
+                                    .font(Font.custom("Rubik", size: 16))
+                                    .foregroundColor(.black)
+                            }
+                            .padding(16)
+                            .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color("Gray 1")))
                         }
-                        .padding(16)
-                        .background(RoundedRectangle(cornerRadius: 6).foregroundColor(Color("Gray 1")))
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.horizontal)
+                        .offset(y: 25)
                     }
-                    .fixedSize(horizontal: false, vertical: true)
-                    .padding(.horizontal)
-                    .offset(y: 25)
-                }
-                .zIndex(2.0)
-                .frame(maxWidth: .infinity, maxHeight: 200)
-                
-                ScrollView {
-                    LazyVStack(alignment: .leading) {
-                        if (!hasSearched) {
-                            Text("Digite o texto da busca e toque em \"Retorno\" para efetuá-la.")
-                                .foregroundColor(Color("Gray 3"))
-                                .font(Font.custom("Rubik", size: 12))
-                                .fontWeight(.light)
-                        } else {
-                            Text(foundResultsText)
-                                .foregroundColor(Color("Gray 3"))
-                                .font(Font.custom("Rubik", size: 12))
-                                .fontWeight(.light)
-                            
-                            ForEach(enterprises.indices, id: \.self) { index in
-                                Text("\(index) \(self.enterprises[index].enterpriseName ?? "")")
+                    .zIndex(2.0)
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    
+                    ScrollView {
+                        LazyVStack(alignment: .leading) {
+                            if (!hasSearched) {
+                                Text("Digite o texto da busca e toque em \"Retorno\" para efetuá-la.")
+                                    .foregroundColor(Color("Gray 3"))
+                                    .font(Font.custom("Rubik", size: 12))
+                                    .fontWeight(.light)
+                            } else {
+                                Text(foundResultsText)
+                                    .foregroundColor(Color("Gray 3"))
+                                    .font(Font.custom("Rubik", size: 12))
+                                    .fontWeight(.light)
+                                
+                                ForEach(enterprises.indices, id: \.self) { index in
+                                    Text("\(index) \(self.enterprises[index].enterpriseName ?? "")")
+                                }
                             }
                         }
+                        .padding(.top, 36)
+                        .padding(.horizontal)
                     }
-                    .padding(.top, 36)
-                    .padding(.horizontal)
                 }
+                .navigationBarHidden(true)
+                .edgesIgnoringSafeArea([.top])
             }
-            .navigationBarHidden(true)
-            .edgesIgnoringSafeArea([.top])
         }
         .navigationBarBackButtonHidden(true)
         .preferredColorScheme(.light)
@@ -87,7 +89,7 @@ struct EnterpriseSearch: View {
                 foundResultsText = "\(enterprises.count) resultado\(plural ? "s" : "") encontrado\(plural ? "s" : "")."
             case .failure(let parsedError):
                 if let _errors = parsedError.errors {
-//                    errors = _errors
+                    //                    errors = _errors
                 }
             }
             loading = false
